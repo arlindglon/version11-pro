79c3d3c409d72615ec2e36e527e1dedd8ee55116
arlindglon <arlindglon@gmail.com>
2026-03-27 09:32:52 +0600

    Update route.ts

--- src/app/api/config/route.ts
+++ src/app/api/config/route.ts
@@ -149,7 +149,7 @@ export function getDefaultConfig() {
     supabase_anon_key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmeWN0eGVsdHRpbXFwcm1uc2tsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDQyNTY3OCwiZXhwIjoyMDkwMDAxNjc4fQ.jXPNpbGbz2DzqWY1vTiFTy20-9gJxMs72gPZy9CpjLs',
     google_client_id: '0jl44v62aqoasds37oani4prs1ihggb4.apps.googleusercontent.com',
     google_client_secret: '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',
-    google_redirect_uri: 'https://shopclient1.vercel.app/api/auth/google/callback',
+    google_redirect_uri: 'https://version11-pro.vercel.app//api/auth/google/callback',
     app_name: ' POS Premium ',
     app_version: 'v10.1.0',
     production_domain: 'https://shopclient1.vercel.app',
